ee484aaf6c3061b6aeaf72824decbe139bbffcc4
Emily Haynes <ehayne00@hotmail.com>
2019-09-27 21:28:52 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,3 @@ def reverse_each_word(string)
  string.split.collect{|word| word.reverse!}.join(" ")
 end
 
-def reverse_each_word(string)
-  string.split.each{|words| words.reverse!}.join(" ") 
-end
